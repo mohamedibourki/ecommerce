d5c8f0cb34b0bb93a7a296658249c27125f4853d
mohamed ibourki <mohamedibourkioffice@gmail.com>
2024-03-31 23:19:38 +0000

    remove auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,18 @@
 import { ThemeProvider } from "@/components/theme-provider/theme-provider";
 import "@/styles/globals.css";
-import { SessionProvider } from "next-auth/react"
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return <main className={`${inter.className}`}>
-    <SessionProvider session={session}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
   </main>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
-import { useSession, signIn, signOut } from "next-auth/react"
 import { Button } from "@/components/ui/button"
 import SideNav from "@/components/nav/SideNav";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if (session) {
-    return <SideNav>
-      <div>
-        Hello {session?.user.name || "User"} ! <br />
-        <Button onClick={() => signOut()}>Sign out</Button>
-      </div>
-    </SideNav>
-  }
-  return <main className={` flex flex-col items-center justify-center h-screen`}>
-    Not signed in
-    <Button  onClick={() => signIn('google')}>Sign in</Button>
-  </main>
+  return <SideNav>
+    <div>
+      Hello !
+    </div>
+  </SideNav>
 }
